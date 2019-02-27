563746d5be0f288414055a766ebb94bd603a5929
cod <you@example.com>
2019-02-27 12:48:49 -0500

    modify app for small screen

---
+++ build/precache-manifest.c5ad5d1534ea30e5abc033f3a4a70c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5942fd30fdcee63d7d05e2fa0956846",
+    "url": "/static/media/film-icon.f5942fd3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7e5519850ec8cc6b03f",
+    "url": "/static/js/main.a7e55198.chunk.js"
+  },
+  {
+    "revision": "4651dec0184f57da2dd0",
+    "url": "/static/js/1.4651dec0.chunk.js"
+  },
+  {
+    "revision": "a7e5519850ec8cc6b03f",
+    "url": "/static/css/main.1b565531.chunk.css"
+  },
+  {
+    "revision": "8f5b5f8548f09931196e6f5556613ff6",
+    "url": "/index.html"
+  }
+];
